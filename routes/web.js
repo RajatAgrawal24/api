c2a9482da0a993ab711c45a60c3659ac042a2732
RajatAgrawal <rag7731@gmail.com>
2024-03-15 21:13:36 +0530

    AddedNewUserApi

--- routes/web.js
+++ routes/web.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
 const UserController = require('../controllers/UserController');
+const {CheckUserAuth} = require('../middleware/auth')
 
 //UserController
 router.get('/getAllUser' , UserController.getAllUser)
 router.post('/userInsert' , UserController.userInsert)
+router.post('/loginUser' , UserController.loginUser)
+router.get('/logOut' , UserController.logOut)
+router.post('/updatePassword' ,CheckUserAuth , UserController.updatePassword)
 
 module.exports = router
